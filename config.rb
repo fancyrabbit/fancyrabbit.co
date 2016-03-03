a9b5782c9a48f8adc3c60a64c807ca2935265ba6
Ken Mickles <ken@37i.net>
2016-03-03 01:16:06 -0500

    Add tech icons + some client info
    * Closes #2
    * Part of #4

--- config.rb
+++ config.rb
@@ -41,11 +41,54 @@ configure :development do
 end
 
 # Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
+helpers do
+  def sorted_clients
+    @sorted_clients ||= data.clients.sort_by { |c| c.name.gsub(/The\s/, '') }
+  end
+
+  # Embed SVG images inline within a Middleman template.
+  # ---
+  # Adapted from the work of James Martin and Steven Harley.
+  # Reference: https://robots.thoughtbot.com/organized-workflow-for-svg
+  # Also: https://gist.github.com/bitmanic/0047ef8d7eaec0bf31bb
+  def inline_svg(filename, options = {})
+    asset = sprockets.find_asset(filename)
+
+    # If the file wasn't found, embed error SVG
+    if asset.nil?
+      return nil
+      %(
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 400 30"
+          width="400px" height="30px"
+        >
+          <text font-size="16" x="8" y="20" fill="#cc0000">
+            Error: '#{filename}' could not be found.
+          </text>
+          <rect
+            x="1" y="1" width="398" height="28" fill="none"
+            stroke-width="1" stroke="#cc0000"
+          />
+        </svg>
+      )
+
+    # If the file was found, parse it, add optional classes, and then embed it
+    else
+      file = asset.source.force_encoding("UTF-8")
+      doc = Nokogiri::HTML::DocumentFragment.parse file
+      svg = doc.at_css "svg"
+
+      if options[:class].present?
+        svg["class"] = options[:class]
+      end
+
+      doc
+    end
+  end
+
+  def icon_tag(icon)
+    inline_svg("icons/#{icon}.svg", class: "icon icon-#{icon}")
+  end
+end
 
 set :css_dir, 'stylesheets'
 
